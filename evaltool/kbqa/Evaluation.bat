c1efc7844e9c0cb5cfb5fb9c1dd752a084c0738f
Dongdong Yang <yang_dong_dong@outlook.com>
2017-10-10 21:49:44 -0700

    Add files via upload

---
+++ evaltool/kbqa/Evaluation.bat
@@ -0,0 +1,2 @@
+REM Usage of Eva.exe: arg1 is the golden file, arg2 is the prediction file
+Eva.exe ..\..\training\nlpcc-iccpol-2016.kbqa.training-data .\nlpcc-iccpol-2016.kbqa.training-data.prediction
